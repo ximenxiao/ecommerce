d5a864d9dbd53757fc5493bfdcd72569a08711bb
Eddy Hu <eddy.hu@yahoo.com>
2019-08-17 16:03:40 -0400

    add api

--- src/main/java/com/demo/ecommerce/controller/UserController.java
+++ src/main/java/com/demo/ecommerce/controller/UserController.java
@@ -3,11 +3,19 @@ package com.demo.ecommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.ecommerce.domain.User;
 import com.demo.ecommerce.service.UserService;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class UserController {
@@ -16,8 +24,38 @@ public class UserController {
 	private UserService userService;
 	
 	@GetMapping("/users")
-	public List<User> hello() {
-		return userService.findAll();
+	public List<User> findAllUsers(
+			@RequestParam(value = "userName", required = false)String userName,
+			@RequestParam(value = "status", required = false) Integer status,
+			@RequestParam(value = "email", required = false) String email,
+			@RequestParam(value = "country", required = false)String country
+			) {
+		return userService.findAll(userName,status,email,country);
 	}
+	
+	@GetMapping("/users/{id}")
+	public User findUserById(@PathVariable("id") Long id) {
+		return userService.findById(id);
+	}
+	
+	@PostMapping("/users")
+	public User addUser(@RequestBody User user) {
+		return userService.add(user);
+	}
+	
+	@PutMapping("/users/{id}")
+	public User updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+		return userService.update(id,user);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public User deleteUser(@PathVariable("id") Long id) {
+		 userService.delete(userService.findById(id));
+		 return null;
+	}
+	
+	
+	
+	
 	
 }
